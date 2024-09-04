47a082b89d3a7ed429f456f86e3510a4f39c954d
Olja2020 <59508997+Olja2020@users.noreply.github.com>
2024-09-04 11:15:41 +0000

    Deploying to gh-pages from @ Olja2020/test@b053ec6f188e5729c3ec91b024d3db52b73d6be7 🚀

---
+++ assets/HomePage-CZLDVlCS.js
@@ -0,0 +1,2 @@
+import{j as e}from"./index-BdrbpL0f.js";const t=()=>e.jsx(e.Fragment,{children:e.jsx("p",{children:"HI, i alone"})});export{t as default};
+//# sourceMappingURL=HomePage-CZLDVlCS.js.map
---
+++ assets/NotFoundPage-BdE5MuTn.js
@@ -0,0 +1,2 @@
+import{j as t,L as o}from"./index-BdrbpL0f.js";const s="_text_mz7f2_1",n="_link_mz7f2_8",e={text:s,link:n};function l(){return t.jsxs("p",{className:e.text,children:["Sorry, page not found! Please go to",t.jsx(o,{to:"/welcome",className:e.link,children:"Welcome Page!"})]})}export{l as default};
+//# sourceMappingURL=NotFoundPage-BdE5MuTn.js.map
